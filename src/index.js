a6a41195573b2ee4ae2b117a56aa0df9586aa103
Austin Abbott <magicaustin51547@gmail.com>
2020-04-27 09:43:22 -0600

    Allow a user to send messages and view them

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//   });
+// });
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, () => {
